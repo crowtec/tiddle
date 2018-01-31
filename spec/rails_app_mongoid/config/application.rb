004f147cc404743ebfa7c13a2acdbb117a2ee712
Carl Vander <calle.vander@gmail.com>
2018-01-31 14:07:02 +0100

    Make tiddle mongoid compatible and add mongoid to travis build matrix.

--- spec/rails_app_mongoid/config/application.rb
+++ spec/rails_app_mongoid/config/application.rb
@@ -1,15 +1,15 @@
 require File.expand_path('../boot', __FILE__)
 
 require "active_model/railtie"
-require "active_record/railtie"
+require "active_job/railtie"
 require "action_controller/railtie"
+require "action_mailer/railtie"
 require "action_view/railtie"
 
 module RailsApp
   class Application < Rails::Application
     config.eager_load = true
-    config.root =  File.expand_path('../../.', __FILE__)
+    config.root = File.expand_path('../../.', __FILE__)
     config.consider_all_requests_local = true
   end
 end
-
